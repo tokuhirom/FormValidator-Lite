aa94750be4b9090480b546d931f7cb60046b37e0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-02-17 13:56:10 +0900

    initial commit

---
+++ lib/OreOre/Validator.pm
@@ -0,0 +1,33 @@
+package OreOre::Validator;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+OreOre::Validator -
+
+=head1 SYNOPSIS
+
+  use OreOre::Validator;
+
+=head1 DESCRIPTION
+
+OreOre::Validator is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

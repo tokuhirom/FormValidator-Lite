f6f5c13d871bbf5d837c3d449eadaad66153eeb2
tokuhirom <tokuhirom@gmail.com>
2010-01-30 16:16:00 +0900

    Checking in changes prior to tagging of version 0.05.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 03e30e7..484d621 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension FormValidator::Lite
    
    +0.05
    +
    +        - added $validator->get_error_messages_from_param($param) method
    +          (by nekokak++)
    +
     0.04
    
             - do not modify arguments in $fvl->check().

--- lib/FormValidator/Lite.pm
+++ lib/FormValidator/Lite.pm
@@ -8,7 +8,7 @@ use Scalar::Util qw/blessed/;
 use FormValidator::Lite::Constraint::Default;
 use FormValidator::Lite::Upload;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 our $Rules;
 our $FileRules;

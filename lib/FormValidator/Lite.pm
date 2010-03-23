3fe41a38ea8794cbc2df5d811cd71f4265faefe9
tokuhirom <tokuhirom@gmail.com>
2010-03-24 00:57:15 +0900

    Checking in changes prior to tagging of version 0.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ece53a4..a174410 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,13 @@
     Revision history for Perl extension FormValidator::Lite
    
    +0.10
    +
    +        - no feature changes
    +
    +0.09
    +
    +        - added docs.
    +
     0.08
    
             - depend to CGI.pm 3.31 or later.

--- lib/FormValidator/Lite.pm
+++ lib/FormValidator/Lite.pm
@@ -8,7 +8,7 @@ use Scalar::Util qw/blessed/;
 use FormValidator::Lite::Constraint::Default;
 use FormValidator::Lite::Upload;
 
-our $VERSION = '0.08';
+our $VERSION = '0.10';
 
 our $Rules;
 our $FileRules;

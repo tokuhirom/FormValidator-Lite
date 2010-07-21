490aab22978f4930341c6a4e411c4286ffdba233
tokuhirom <tokuhirom@gmail.com>
2010-07-21 14:53:40 +0900

    Checking in changes prior to tagging of version 0.17.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c418b5b..459eeff 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension FormValidator::Lite
    
    +0.17
    +
    +        - (no feature changes)
    +        - fixed testing issue.
    +
     0.16
    
             - $v->get_error_messages() should return arrayref in scalar context.

--- lib/FormValidator/Lite.pm
+++ lib/FormValidator/Lite.pm
@@ -8,7 +8,7 @@ use Scalar::Util qw/blessed/;
 use FormValidator::Lite::Constraint::Default;
 use FormValidator::Lite::Upload;
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 our $Rules;
 our $FileRules;

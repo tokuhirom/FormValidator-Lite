ba3c291b618b5c57b3e7d2905e29904757abca8b
Toshihiro Sugimoto <toshihiro0412@gmail.com>
2021-08-05 19:19:57 +0900

    update POD

--- lib/FormValidator/Lite/Constraint/Default.pm
+++ lib/FormValidator/Lite/Constraint/Default.pm
@@ -227,5 +227,13 @@ FILTER is special constraint. It does not check the value and simply filter.
 "trim" is only pre-defined. You can also pass a callback.
 Callback takes parameter as first argument, should return filtered value.
 
+=item FLAG
+
+    $validator->check(
+        is_enabled => [qw/FLAG/]
+    );
+
+Check parameter is 0 or 1.
+
 =back
 
